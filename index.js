6780a3364169bc32854c3f4ad2b624bd6c1a1da6
Tushar Bhumkar <tusharbhumkar25@gmail.com>
2021-06-08 18:27:23 +0530

    made structure working

--- index.js
+++ index.js
@@ -10,21 +10,44 @@ app.get('/',(req,res)=>{
     res.send('Todo App');
 });
 
-app.get('/read',(req,res)=>{
-    let arr = ['t1','t2','t3'];
-    res.send(JSON.stringify(arr));
+app.get('/read/:id',(req,res)=>{
+    var data = JSON.parse(fs.readFileSync('todo_list.json','utf8'));
+    let task = data.find(o => o.task_number == req.params.id);
+    res.json(task);
 });
 
 app.post('/create',(req,res)=>{
-    res.json(req.body);
+    var data = JSON.parse(fs.readFileSync('todo_list.json','utf8'));
+    let max = Math.max.apply(Math, data.map(function(o) { return o.task_number; }))
+    max = (max>0)?max:0;
+    let todo = req.body;
+    todo.task_number = parseInt(max)+1;
+    data.push(todo);
+    fs.writeFileSync('todo_list.json',JSON.stringify(data));
+    res.json(todo);
 });
 
-app.patch('/update',(req,res)=>{
-    res.json(req.body);
+app.patch('/update/:id',(req,res)=>{
+    var data = JSON.parse(fs.readFileSync('todo_list.json','utf8'));
+    data = data.filter(function( obj ) {
+        return obj.task_number != req.params.id;
+    });
+    let max = Math.max.apply(Math, data.map(function(o) { return o.task_number; }))
+    max = (max>0)?max:0;
+    let todo = req.body;
+    todo.task_number = parseInt(max==null?0:max)+1;
+    data.push(todo);
+    fs.writeFileSync('todo_list.json',JSON.stringify(data));
+    res.json(data);
 });
 
-app.delete('/delete',(req,res)=>{
-    res.json(req.body);
+app.delete('/delete/:id',(req,res)=>{
+    var data = JSON.parse(fs.readFileSync('todo_list.json','utf8'));
+    data = data.filter(function( obj ) {
+        return obj.task_number != req.params.id;
+    });
+    fs.writeFileSync('todo_list.json',JSON.stringify(data));
+    res.json(data);
 });
 
 app.listen(3000);
